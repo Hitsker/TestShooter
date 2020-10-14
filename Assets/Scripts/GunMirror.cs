c8e2ededd6a2cf1182eeca153a6dc8c28dca3186
Ekaterina Kazakevich <ekaterinaka@playtika.com>
2020-10-14 18:24:39 +0300

    Mirror

---
+++ Assets/Scripts/GunMirror.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunMirror : MonoBehaviour
+{
+    [SerializeField] private Renderer _renderer;
+    [SerializeField] private Camera _mirrorCamera;
+    private RenderTexture rt;
+
+    void  Start()
+    {
+        rt = new RenderTexture(256, 256, 16, RenderTextureFormat.ARGB32);
+        rt.Create();
+        _mirrorCamera.targetTexture = rt;
+        var newMat = new Material(Shader.Find("Diffuse")) {mainTexture = rt};
+        _renderer.material = newMat;
+    }
+}
